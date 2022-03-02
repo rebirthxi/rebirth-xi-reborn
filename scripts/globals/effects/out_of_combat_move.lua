2a1044704c3f2b3bde0c25cbf36d7497fd5d04d2
Daniel Koris <korisd@gmail.com>
2022-03-01 18:10:37 -0700

    base: ooc_move: added out of combat movement

---
+++ scripts/globals/effects/out_of_combat_move.lua
@@ -0,0 +1,17 @@
+-----------------------------------
+-- xi.effect.OUT_OF_COMBAT_MOVE
+-----------------------------------
+local effect_object = {}
+
+effect_object.onEffectGain = function(target, effect)
+    target:addMod(xi.mod.MOVE, effect:getPower())
+end
+
+effect_object.onEffectTick = function(target, effect)
+end
+
+effect_object.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.MOVE, effect:getPower())
+end
+
+return effect_object
