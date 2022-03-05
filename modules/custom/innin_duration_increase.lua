5ae26f1e9e496b0a0595d40152f8b9de8fa5ae5c
Daniel Koris <korisd@gmail.com>
2022-03-04 19:11:02 -0700

    jobs: ninja: added a module to increase Innin duration to one hour

---
+++ modules/custom/innin_duration_increase.lua
@@ -0,0 +1,15 @@
+-----------------------------------
+-- Change the Duration of Innin
+-----------------------------------
+require("modules/module_utils")
+-----------------------------------
+local m = Module:new("innin_duration_increase")
+m:setEnabled(true)
+
+m:addOverride("xi.globals.abilities.innin.onUseAbility", function(player, target, ability)
+    target:delStatusEffect(xi.effect.INNIN)
+    target:delStatusEffect(xi.effect.YONIN)
+    target:addStatusEffect(xi.effect.INNIN, 30, 15, 3600, 0, 20)
+end)
+
+return m
