e70199206c59946ef5050c72305b44f5417bc5dd
Daniel Koris <korisd@gmail.com>
2022-03-04 19:12:21 -0700

    jobs: ninja: added a module to increase Yonin duration to one hour

---
+++ modules/custom/yonin_duration_increase.lua
@@ -0,0 +1,15 @@
+-----------------------------------
+-- Change the Duration of Yonin
+-----------------------------------
+require("modules/module_utils")
+-----------------------------------
+local m = Module:new("yonin_duration_increase")
+m:setEnabled(true)
+
+m:addOverride("xi.globals.abilities.yonin.onUseAbility", function(player, target, ability)
+    target:delStatusEffect(xi.effect.INNIN)
+    target:delStatusEffect(xi.effect.YONIN)
+    target:addStatusEffect(xi.effect.YONIN, 30, 15, 3600, 0, 20)
+end)
+
+return m
