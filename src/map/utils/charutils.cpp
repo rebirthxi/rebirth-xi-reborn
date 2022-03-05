f753895b6750674fa6bc932d39c0d635a730be8c
Daniel Koris <korisd@gmail.com>
2022-03-04 18:21:11 -0700

    jobs: paladin: added provoke to Paladin at level 10 as main and sub job

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -2635,6 +2635,10 @@ namespace charutils
 
         AbilitiesList = ability::GetAbilities(PChar->GetMJob());
 
+        // RebirthXI Hack in Paladin Provoke for Paladin as Main job
+        if (PChar->GetMJob() == JOB_PLD and PChar->GetMLevel() >= 10)
+            addAbility(PChar, 35);
+
         for (auto PAbility : AbilitiesList)
         {
             if (PAbility == nullptr)
@@ -2674,6 +2678,10 @@ namespace charutils
         }
 
         AbilitiesList = ability::GetAbilities(PChar->GetSJob());
+
+        // RebirthXI Hack in Paladin Provoke for Paladin as Sub job
+        if (PChar->GetSJob() == JOB_PLD and PChar->GetSLevel() >= 10)
+            addAbility(PChar, 35);
 
         for (auto PAbility : AbilitiesList)
         {
