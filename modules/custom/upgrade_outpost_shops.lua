7ce724a8a8bb82d43edb6cc79fa73aae630eae5c
Daniel Koris <korisd@gmail.com>
2022-03-11 19:36:48 -0700

    module: upgrade outpost shops: added rebirth 2.0 stock to the outpost shop
    Thief's Tools
    Silent Oils
    Prism Powder
    
    At a decently fair, but emergency, price.

---
+++ modules/custom/upgrade_outpost_shops.lua
@@ -0,0 +1,29 @@
+-----------------------------------
+-- Upgrade the stock in Outposts
+-- * Add Sneak Oils / Prism Powder
+-- * Add Keys
+-- Prices will be decently high
+-----------------------------------
+require("modules/module_utils")
+require("scripts/globals/shop")
+
+-----------------------------------
+local m = Module:new("upgrade_outpost_shops")
+m:setEnabled(true)
+
+m:addOverride("xi.shop.outpost", function(player)
+    local stock =
+    {
+        4148,  316, -- Antidote
+        4151,  800, -- Echo Drops
+        4128, 4832, -- Ether
+        4150, 2595, -- Eye Drops
+        4112,  910, -- Potion
+        1022, 4500, -- Thief's Tools
+        4165, 7500, -- Silent Oil
+        4164, 7500, -- Prism Powder
+    }
+    xi.shop.general(player, stock)
+end)
+
+return m
