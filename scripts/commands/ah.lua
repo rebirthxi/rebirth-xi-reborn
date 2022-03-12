31199ca771747a821491c75e23aaf1077f1fd023
Daniel Koris <korisd@gmail.com>
2022-03-11 19:20:57 -0700

    commands: ah: added the rebirth 2.0 ah command

--- scripts/commands/ah.lua
+++ scripts/commands/ah.lua
@@ -2,13 +2,24 @@
 -- func: ah
 -- desc: opens the Auction House menu anywhere in the world
 -----------------------------------
+require("scripts/globals/zone")
 
 cmdprops =
 {
-    permission = 1,
+    permission = 0,
     parameters = ""
 }
 
 function onTrigger(player)
-    player:sendMenu(3)
+    local zone = player:getZone()
+
+    if player:getGMLevel() > 0 then
+        player:sendMenu(3)
+    elseif zone:getType() == xi.zoneType.CITY then
+        player:sendMenu(3)
+    elseif player:hasTeleport(player:getNation(), zone:getRegionID() + 5) then
+        player:sendMenu(3)
+    else
+        player:PrintToPlayer("[AH Command] You do not meet the requirements to use this command.", 0xF)
+    end
 end
