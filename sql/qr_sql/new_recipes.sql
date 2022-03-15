7cddb9808a87e9d365eb8abb4aa14eb61688ab05
Daniel Koris <korisd@gmail.com>
2022-03-14 19:17:10 -0600

    recipes: added the staff and wotg breastplate recipes

---
+++ sql/qr_sql/new_recipes.sql
@@ -0,0 +1,14 @@
+-- Iridial Staff
+INSERT INTO synth_recipes VALUE (NULL, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 4102, 4244, 17545, 17547, 17549, 17551, 17553, 17555, 17557, 17559, 18632, 18632, 18632, 18632, 1, 1, 1, 1,'Iridal Staff');
+
+-- Chatoyant Staff
+INSERT INTO synth_recipes VALUE (NULL, 0, 0, 90, 0, 0, 0, 0, 0, 0, 0, 4102, 4244, 17546, 17548, 17550, 17552, 17554, 17556, 17558, 17560, 18633, 18633, 18633, 18633, 1, 1, 1, 1,'Chatoyant Staff');
+
+-- Antares Harness
+INSERT INTO synth_recipes VALUE (NULL,0,0,0,95,0,0,0,0,0,0,4099,4241,13714,13734,0,0,0,0,0,0,11287,0,0,0,1,0,0,0,'Antares Harness');
+
+-- Avalon Breastplate
+INSERT INTO synth_recipes VALUES (NULL,0,0,0,0,0,0,0,0,95,0,4099,4241,13766,13790,0,0,0,0,0,0,11286,0,0,0,1,0,0,0,'Avalon Breastplate');
+
+-- Zahak's Mail
+INSERT INTO synth_recipes VALUES (NULL,0,0,0,0,0,0,0,95,0,0,4099,4241,13735,14341,0,0,0,0,0,0,11288,0,0,0,1,0,0,0,'Zahak Mail');
--- tools/db_update.py
+++ tools/db_update.py
@@ -87,6 +87,7 @@ safe_files_to_load = [
     "sql/qr_sql/helm_gear_mods.sql",
     "sql/qr_sql/qr_augments.sql",
     "sql/qr_sql/beastman_bosses.sql",
+    "sql/qr_sql/new_recipes.sql",
 ]
 
 for sql_file in safe_files_to_load:
