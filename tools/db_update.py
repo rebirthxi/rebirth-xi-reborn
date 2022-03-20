d730091cc31f4ee59f5b60517a727743078d633e
Daniel Koris <korisd@gmail.com>
2022-03-20 15:51:17 -0600

    db: triggers: added a file for reborn specific triggers that modify the existing lsb ones

--- tools/db_update.py
+++ tools/db_update.py
@@ -88,6 +88,7 @@ safe_files_to_load = [
     "sql/qr_sql/qr_augments.sql",
     "sql/qr_sql/beastman_bosses.sql",
     "sql/qr_sql/new_recipes.sql",
+    "sql/qr_sql/qr_triggers.sql",
 ]
 
 for sql_file in safe_files_to_load:
