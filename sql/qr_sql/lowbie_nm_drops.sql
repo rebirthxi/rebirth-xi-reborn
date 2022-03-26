0517968824cb29003e28f5e018e1355c0a29ed4f
Daniel Koris <korisd@gmail.com>
2022-03-25 20:20:38 -0600

    nms: lowbie nms: changed leaping lizzy and valkurm emporer to be more friendly... all around

---
+++ sql/qr_sql/lowbie_nm_drops.sql
@@ -0,0 +1,3 @@
+-- Increase drop rate for BB and EH
+UPDATE mob_droplist SET itemRate=250 WHERE itemId=15351; -- Bounding Boots
+UPDATE mob_droplist SET itemRate=250 WHERE itemId=15224; -- Empress Hairpin
