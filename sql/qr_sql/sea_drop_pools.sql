d726157c88f97ee4de4b10fab792b6f98c194e09
Daniel Koris <korisd@gmail.com>
2022-03-31 21:11:32 -0600

    sea: added classic era drop pools for sea NMs

---
+++ sql/qr_sql/sea_drop_pools.sql
@@ -0,0 +1,67 @@
+-- Ix Mnk
+DELETE FROM mob_droplist WHERE dropId=2845;
+INSERT INTO mob_droplist VALUES (2845, 0, 0, 1000, 1851, 453); -- Deed of Placidity
+INSERT INTO mob_droplist VALUES (2845, 0, 0, 1000, 1901, 50); -- Vice of Antipathy
+
+
+-- Ix Drk
+DELETE FROM mob_droplist WHERE dropId=2844;
+INSERT INTO mob_droplist VALUES (2844, 0, 0, 1000, 1854, 453); -- Deed of Moderation
+INSERT INTO mob_droplist VALUES (2844, 0, 0, 1000, 1902, 50); -- Vice of Avarice
+
+
+-- Ix Drg
+DELETE FROM mob_droplist WHERE dropId=2843;
+INSERT INTO mob_droplist VALUES (2843, 0, 0, 1000, 1870, 453); -- Deed of Sensibility
+INSERT INTO mob_droplist VALUES (2843, 0, 0, 1000, 1903, 50); -- Vice of Aspersion
+
+
+-- Jailer of Temperance
+DELETE FROM mob_droplist WHERE dropId=1402;
+INSERT INTO mob_droplist VALUES (1402, 0, 0, 1000, 1850, 90); -- First Virtue
+INSERT INTO mob_droplist VALUES (1402, 0, 0, 1000, 17948, 1000); -- Temperance Axe
+INSERT INTO mob_droplist VALUES (1402, 0, 0, 1000, 15513, 1000); -- Temperance Torque
+
+
+-- Jailer of Fortitude
+DELETE FROM mob_droplist WHERE dropId=1397;
+INSERT INTO mob_droplist VALUES (1397, 0, 0, 1000, 18360, 1000); -- Fortitude Axe
+INSERT INTO mob_droplist VALUES (1397, 0, 0, 1000, 15512, 90); -- Fortitude Torque
+INSERT INTO mob_droplist VALUES (1397, 0, 0, 1000, 1853, 1000); -- Second Virtue
+
+
+-- Jailer of Faith
+DELETE FROM mob_droplist WHERE dropId=1396;
+INSERT INTO mob_droplist VALUES (1396, 0, 0, 1000, 18360, 1000); -- Faith Baghnaks
+INSERT INTO mob_droplist VALUES (1396, 0, 0, 1000, 15512, 90); -- Faith Torque
+INSERT INTO mob_droplist VALUES (1396, 0, 0, 1000, 1856, 1000); -- Third Virtue
+
+
+-- Jailer of Justice
+DELETE FROM mob_droplist WHERE dropId=1399;
+INSERT INTO mob_droplist VALUES (1399, 0, 0, 1000, 17710, 1000); -- Justice Sword
+INSERT INTO mob_droplist VALUES (1399, 0, 0, 1000, 15508, 90); -- Justice Torque
+INSERT INTO mob_droplist VALUES (1399, 0, 0, 1000, 1848, 1000); -- Fourth Virtue
+
+
+-- Jailer of Hope
+DELETE FROM mob_droplist WHERE dropId=1398;
+INSERT INTO mob_droplist VALUES (1398, 0, 0, 1000, 17595, 1000); -- Hope Staff
+INSERT INTO mob_droplist VALUES (1398, 0, 0, 1000, 15509, 90); -- Hope Torque
+INSERT INTO mob_droplist VALUES (1398, 0, 0, 1000, 1847, 1000); -- Fifth Virtue
+
+
+-- Jailer of Prudence
+DELETE FROM mob_droplist WHERE dropId=1401;
+INSERT INTO mob_droplist VALUES (1401, 0, 0, 1000, 1849, 1000); -- Sixth Virtue
+INSERT INTO mob_droplist VALUES (1401, 0, 0, 1000, 18397, 1000); -- Prudence Rod
+INSERT INTO mob_droplist VALUES (1401, 0, 0, 1000, 15510, 90); -- Prudence Torque
+
+
+-- Jailer of Love
+DELETE FROM mob_droplist WHERE dropId=1400;
+INSERT INTO mob_droplist VALUES (1400, 0, 0, 1000, 1911, 284); -- Aura of Adulation
+INSERT INTO mob_droplist VALUES (1400, 0, 0, 1000, 1912, 246); -- Aura of Voracity
+INSERT INTO mob_droplist VALUES (1400, 0, 0, 1000, 18100, 1000); -- Love Halberd
+INSERT INTO mob_droplist VALUES (1400, 0, 0, 1000, 15514, 161); -- Love Torque
+
