ac2e42c3b138c83e3cc87e7c428687ee8491367a
Daniel Koris <korisd@gmail.com>
2022-03-31 20:38:31 -0600

    sky: updated sky NMs drop pools

---
+++ sql/qr_sql/sky_drop_pools.sql
@@ -0,0 +1,140 @@
+-- Despot
+DELETE FROM mob_droplist WHERE dropId=638;
+INSERT INTO mob_droplist VALUES (638, 0, 0, 1000, 1422, 1000); -- Gem of the West
+INSERT INTO mob_droplist VALUES (638, 0, 0, 1000, 1442, 228); -- Oblation Abjuration
+INSERT INTO mob_droplist VALUES (638, 0, 0, 1000, 18044, 102); -- Scarecrow Scythe
+
+-- Brigandish Blade
+DELETE FROM mob_droplist WHERE dropId=357;
+INSERT INTO mob_droplist VALUES (357, 0, 0, 1000, 1420, 1000); -- Gem of the South
+INSERT INTO mob_droplist VALUES (357, 0, 0, 1000, 1441, 262); -- Libation Abjuration
+
+-- Steam Cleaner
+DELETE FROM mob_droplist WHERE dropId=2326;
+INSERT INTO mob_droplist VALUES (2326, 0, 0, 1000, 1445, 32); -- Freya's Tear
+INSERT INTO mob_droplist VALUES (2326, 0, 0, 1000, 1418, 1000); -- Gem of the East
+INSERT INTO mob_droplist VALUES (2326, 0, 0, 1000, 17511, 98); -- Indra Katars
+INSERT INTO mob_droplist VALUES (2326, 0, 0, 1000, 1441, 251); -- Libration Abjuration
+
+-- Zipacna
+DELETE FROM mob_droplist WHERE dropId=2800;
+INSERT INTO mob_droplist VALUES (2800, 0, 0, 1000, 1424, 1000); -- Gem of the North
+INSERT INTO mob_droplist VALUES (2800, 0, 0, 1000, 1442, 213); -- Oblation Abjuration
+INSERT INTO mob_droplist VALUES (2800, 0, 0, 1000, 17804, 113); -- Ushikirimaru
+
+-- Faust
+DELETE FROM mob_droplist WHERE dropId=2821;
+INSERT INTO mob_droplist VALUES (2821, 0, 0, 1000, 1442, 192); -- Oblation Abjuration
+INSERT INTO mob_droplist VALUES (2821, 0, 0, 1000, 1421, 1000); -- Summerstone
+INSERT INTO mob_droplist VALUES (2821, 0, 0, 1000, 16838, 115); -- Tonbo-Giri
+
+-- Mother Globe
+DELETE FROM mob_droplist WHERE dropId=2820;
+INSERT INTO mob_droplist VALUES (2820, 0, 0, 1000, 1419, 1000); -- Springstone
+INSERT INTO mob_droplist VALUES (2820, 0, 0, 1000, 1441, 215); -- Libation Abjuration
+INSERT INTO mob_droplist VALUES (2820, 0, 0, 1000, 17774, 154); -- Shiranui
+
+-- Olla Grande
+DELETE FROM mob_droplist WHERE dropId=2823;
+INSERT INTO mob_droplist VALUES (2823, 0, 0, 1000, 1445, 32); -- Freya's Tear
+INSERT INTO mob_droplist VALUES (2823, 0, 0, 1000, 1441, 215); -- Libration Abjuration
+INSERT INTO mob_droplist VALUES (2823, 0, 0, 1000, 16956, 108); -- Skofnung
+INSERT INTO mob_droplist VALUES (2823, 0, 0, 1000, 1425, 1000); -- Winterstone
+
+-- Ullikummi
+DELETE FROM mob_droplist WHERE dropId=2822;
+INSERT INTO mob_droplist VALUES (2822, 0, 0, 1000, 1423, 1000); -- Autumnstone
+INSERT INTO mob_droplist VALUES (2822, 0, 0, 1000, 1442, 180); -- Oblation Abjuration
+INSERT INTO mob_droplist VALUES (2822, 0, 0, 1000, 18199, 100); -- Ulfhedinn Axe
+
+-- Byakko
+DELETE FROM mob_droplist WHERE dropId=394;
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1406, 1000); -- Seal of Byakko
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 18163, 1000); -- Zephyr
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1110, 25); -- Beetle Blood
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 860, 180); -- Behemoth Hide
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 658, 12); -- D.Ingot
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 722, 106); -- D.Log
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1311, 23); -- Oxblood
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 837, 126); -- Malboro Fiber
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 18198, 230); -- Byakko's Axe
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 12818, 197); -- Byakko's Haidate
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1324, 138); -- A.Head
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1317, 129); -- D.Legs
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1323, 143); -- E.Feet
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 1341, 188); -- N.Hands
+INSERT INTO mob_droplist VALUES (394, 0, 0, 1000, 2438, 162); -- H.Feet
+
+-- Genbu
+DELETE FROM mob_droplist WHERE dropId=946;
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1404, 1000); -- Seal of Genbu
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 18161, 1000); -- Arctic Wind
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 655, 22); -- Adaman Ingot
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 908, 99); -- Adamantoise Shell
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1110, 24); -- Beetle Blood
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 722, 6); -- D.Log
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 901, 203); -- V.Claw
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1311, 48); -- Oxblood
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 12296, 248); -- Genbu's Shield
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 12434, 220); -- Genbu's Kabuto
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1326, 126); -- A.Hands
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1324, 138); -- A.Head
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1331, 164); -- M.Hands
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 1338, 169); -- W.Feet
+INSERT INTO mob_droplist VALUES (946, 0, 0, 1000, 2434, 158); -- H.Head
+
+-- Seiryu
+DELETE FROM mob_droplist WHERE dropId=2196;
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1405, 1000); -- Seal of Seiryu
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 18162, 1000); -- East Wind
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1312, 1); -- Angel Skin
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1110, 10); -- Beetle Blood
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 836, 39); -- D.Cloth
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1133, 116); -- Dragon Blood
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 4486, 13); -- Dragon Heart
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 4272, 92); -- Dragon Meat
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 903, 126); -- Dragon Talon
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 837, 30); -- Malboro Fiber
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1311, 10); -- Oxblood
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 12690, 225); -- Seiryu's Kote
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 17659, 202); -- Seiryu's Sword
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1327, 119); -- A.Legs
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1314, 122); -- D.Head
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1329, 159); -- M.Head
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 1336, 195); -- W.Hands
+INSERT INTO mob_droplist VALUES (2196, 0, 0, 1000, 2431, 171); -- P.Hands
+
+-- Suzaku
+DELETE FROM mob_droplist WHERE dropId=2362;
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1407, 1000); -- Seal of Suzaku
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 18164, 1000); -- Arctic Wind
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1110, 96); -- Beetle Blood
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 836, 123); -- D.Cloth
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 747, 31); -- O.Ingot
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1313, 103); -- Siren's Hair
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1313, 103); -- Siren's Hair
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1313, 103); -- Siren's Hair
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 901, 11); -- V.Claw
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 18043, 243); -- Suzaku's Scythe
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 12946, 239); -- Suzaku's Sune-Ate
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1327, 102); -- A.Legs
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1316, 154); -- D.Hands
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1319, 137); -- E.Head
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 1343, 192); -- N.Feet
+INSERT INTO mob_droplist VALUES (2362, 0, 0, 1000, 2429, 163); -- P.Head
+
+
+-- Kirin
+DELETE FROM mob_droplist WHERE dropId=2819;
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 12562, 196); -- Kirin's Osode
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 17567, 238); -- Kirin's Pole
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 1315, 163); -- D.Body
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 1340, 155); -- N.Body
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 2430, 98); -- P.Body
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 2435, 135); -- H.Body
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 1337, 146); -- W.Legs
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 4818, 61); -- Scroll of Quake
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 4750, 65); -- Scroll of Reraise III
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 747, 138); -- O.Ingot
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 831, 184); -- Shining Cloth
+INSERT INTO mob_droplist VALUES (2819, 0, 0, 1000, 658, 169); -- D.Ingot
