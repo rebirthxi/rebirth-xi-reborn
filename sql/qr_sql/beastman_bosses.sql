011acd8e2accfe1eca97b6a255e59d4b3b2de0c8
Daniel Koris <korisd@gmail.com>
2022-04-12 19:18:42 -0600

    qr_sql: added Nexus Cape to the beastman king's drop pools

--- sql/qr_sql/beastman_bosses.sql
+++ sql/qr_sql/beastman_bosses.sql
@@ -1,18 +1,13 @@
 -- Level Buff
-UPDATE mob_groups SET minLevel=90, maxLevel=90 WHERE name='Za_Dha_Adamantking' and zoneid=148;
-UPDATE mob_groups SET minLevel=90, maxLevel=90 WHERE name='Tzee_Xicu_the_Manifest' and zoneid=151;
-UPDATE mob_groups SET minLevel=90, maxLevel=90 WHERE name='Overlord_Bakgodek' and zoneid=150;
+UPDATE mob_groups SET minLevel=95, maxLevel=95 WHERE name='Za_Dha_Adamantking' and zoneid=148;
+UPDATE mob_groups SET minLevel=95, maxLevel=95 WHERE name='Tzee_Xicu_the_Manifest' and zoneid=151;
+UPDATE mob_groups SET minLevel=95, maxLevel=95 WHERE name='Overlord_Bakgodek' and zoneid=150;
 
 -- Droppools
 
 -- Zha'Dha (dropId: 2830)
 DELETE FROM mob_droplist WHERE dropId=2830;
 INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 11538, 1000); -- Nexus Cape
-INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 17584, 1000); -- Signet Staff
-INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 17584, 100);
-INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 17584, 50);
-INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 1525, 200); -- Adamantoise Egg
-INSERT INTO mob_droplist VALUES (2830, 0, 0, 1000, 14429, 350); -- Republic Aketon
 INSERT INTO `mob_droplist` VALUES (2830,0,0,1000,17073,70);
 INSERT INTO `mob_droplist` VALUES (2830,0,0,1000,658,70);
 INSERT INTO `mob_droplist` VALUES (2830,0,0,1000,703,170);
@@ -29,12 +24,7 @@ INSERT INTO `mob_droplist` VALUES (2830,0,0,1000,1132,130);
 
 -- Tzee Xicu (dropId: 2511)
 DELETE FROM mob_droplist WHERE dropId=2511;
-INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 17585, 1000); -- Signet Staff
 INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 11538, 1000); -- Nexus Cape
-INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 17585, 100);
-INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 17585, 50);
-INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 1526, 200); -- Wyrm Beard
-INSERT INTO mob_droplist VALUES (2511, 0, 0, 1000, 14430, 350); -- Federation Aketon
 INSERT INTO `mob_droplist` VALUES (2511,0,0,1000,17528,150);
 INSERT INTO `mob_droplist` VALUES (2511,0,0,1000,17619,930);
 INSERT INTO `mob_droplist` VALUES (2511,0,0,1000,658,50);
@@ -54,12 +44,7 @@ INSERT INTO `mob_droplist` VALUES (2511,0,0,1000,1132,150);
 
 -- Bakgodek (dropId: 1966)
 DELETE FROM mob_droplist WHERE dropId=1966;
-INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 17583, 1000); -- Signet Staff
 INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 11538, 1000); -- Nexus Cape
-INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 17583, 100);
-INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 17583, 50);
-INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 1527, 200); -- Behemoth Tongue
-INSERT INTO mob_droplist VALUES (1966, 0, 0, 1000, 14428, 350); -- Kingdom Aketon
 INSERT INTO `mob_droplist` VALUES (1966,0,0,1000,12362,190);
 INSERT INTO `mob_droplist` VALUES (1966,0,0,1000,17649,810);
 INSERT INTO `mob_droplist` VALUES (1966,0,0,1000,17928,70);
