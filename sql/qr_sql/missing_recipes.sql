20ab1ebd0e7f9b4c46954004dabff269a1bfcb11
Daniel Koris <korisd@gmail.com>
2022-05-22 09:18:16 -0600

    recipes: added Coral Bangles desnyth

--- sql/qr_sql/missing_recipes.sql
+++ sql/qr_sql/missing_recipes.sql
@@ -1,8 +1,15 @@
 -- Gold Ring Desynth to Gold Ingot
-
 INSERT INTO synth_recipes (Desynth, KeyItem, Wood, Smith, Gold, Cloth, Leather, Bone, Alchemy, Cook, Crystal,
                            HQCrystal, Ingredient1, Ingredient2, Ingredient3, Ingredient4, Ingredient5, Ingredient6,
                            Ingredient7, Ingredient8, Result, ResultHQ1, ResultHQ2, ResultHQ3, ResultQty,
                            ResultHQ1Qty, ResultHQ2Qty, ResultHQ3Qty, ResultName)
 VALUES (1, 0, 0, 0, 60, 0, 0, 0, 0, 0, 4100, 4242, 13445, 0, 0, 0, 0, 0, 0, 0, 745, 745, 745, 745, 1, 1, 2, 2,
         'Gold Ingot');
+
+-- Coral Bangles Desynth to Giant Femur or Coral Fragment
+INSERT INTO synth_recipes (Desynth, KeyItem, Wood, Smith, Gold, Cloth, Leather, Bone, Alchemy, Cook, Crystal,
+                           HQCrystal, Ingredient1, Ingredient2, Ingredient3, Ingredient4, Ingredient5, Ingredient6,
+                           Ingredient7, Ingredient8, Result, ResultHQ1, ResultHQ2, ResultHQ3, ResultQty,
+                           ResultHQ1Qty, ResultHQ2Qty, ResultHQ3Qty, ResultName)
+VALUES (1, 0, 0, 0, 0, 0, 0, 87, 0, 0, 4100, 4242, 13987, 0, 0, 0, 0, 0, 0, 0, 893, 893, 887, 887, 1, 1, 1, 2,
+        'Coral Bangles (desynth)');
