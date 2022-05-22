e58aad056812f457c7ecd41a9316c1758da1261d
Daniel Koris <korisd@gmail.com>
2022-05-22 09:11:05 -0600

    recipes: added gold ring desynth into gold ingot

---
+++ sql/qr_sql/missing_recipes.sql
@@ -0,0 +1,8 @@
+-- Gold Ring Desynth to Gold Ingot
+
+INSERT INTO synth_recipes (Desynth, KeyItem, Wood, Smith, Gold, Cloth, Leather, Bone, Alchemy, Cook, Crystal,
+                           HQCrystal, Ingredient1, Ingredient2, Ingredient3, Ingredient4, Ingredient5, Ingredient6,
+                           Ingredient7, Ingredient8, Result, ResultHQ1, ResultHQ2, ResultHQ3, ResultQty,
+                           ResultHQ1Qty, ResultHQ2Qty, ResultHQ3Qty, ResultName)
+VALUES (1, 0, 0, 0, 60, 0, 0, 0, 0, 0, 4100, 4242, 13445, 0, 0, 0, 0, 0, 0, 0, 745, 745, 745, 745, 1, 1, 2, 2,
+        'Gold Ingot');
