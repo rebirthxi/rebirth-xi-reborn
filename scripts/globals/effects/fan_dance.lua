1833cd1cb3d1ed23bca5208868612d5f0d5de133
Daniel Koris <korisd@gmail.com>
2022-05-30 14:46:56 -0600

    [effect] added the physical damage component of Fan Dance
    Still needs the Waltz delay bits.
    
    https://www.bg-wiki.com/ffxi/Fan_Dance

--- scripts/globals/effects/fan_dance.lua
+++ scripts/globals/effects/fan_dance.lua
@@ -12,6 +12,17 @@ effect_object.onEffectGain = function(target, effect)
     target:delStatusEffect(xi.effect.DRAIN_SAMBA)
     target:delStatusEffect(xi.effect.SABER_DANCE)
     target:addMod(xi.mod.ENMITY, 15)
+    target:addMod(xi.mod.UDMGPHYS, effect:getPower())
+    target:addListener("ATTACKED", "FAN_DANCE_DMG_REDUCE", function(dancer, attacker)
+        local fanDanceEffect = dancer:getStatusEffect(xi.effect.FAN_DANCE)
+        local power = fanDanceEffect:getPower()
+        local decayby = 1000
+        print("Power is " .. power)
+        if power > 2000 then
+            fanDanceEffect:setPower(power - decayby)
+            dancer:delMod(xi.mod.UDMGPHYS, decayby)
+        end
+    end)
 end
 
 effect_object.onEffectTick = function(target, effect)
@@ -19,6 +30,8 @@ end
 
 effect_object.onEffectLose = function(target, effect)
     target:delMod(xi.mod.ENMITY, 15)
+    target:delMod(xi.mod.UDMGPHYS, effect:getPower())
+    target:removeListener("FAN_DANCE_DMG_REDUCE")
 end
 
 return effect_object
