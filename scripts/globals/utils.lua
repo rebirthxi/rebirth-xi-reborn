e8918449422d4f412684cace40355df2c8f688c2
Daniel Koris <korisd@gmail.com>
2022-06-12 13:57:13 -0600

    [lua] added helpers for dealing with converting relative positions of a mob to absolute in world space

--- scripts/globals/utils.lua
+++ scripts/globals/utils.lua
@@ -551,6 +551,26 @@ function utils.mobTeleport(mob, hideDuration, pos, disAnim, reapAnim)
     end)
 end
 
+local ffxiRotConversionFactor = 360.0 / 255.0
+
+function utils.ffxiRotToDegrees(ffxiRot)
+    return ffxiRotConversionFactor * ffxiRot
+end
+
+
+function utils.lateralTranslateWithOriginRotation(origin, translation)
+    local degrees = utils.ffxiRotToDegrees(origin.rot)
+    local rads = math.rad(degrees)
+    local new_coords = {}
+
+    new_coords.x = origin.x + ((math.cos(rads) * translation.x) + (math.sin(rads) * translation.z))
+    new_coords.z = origin.z + ((math.cos(rads) * translation.z) - (math.sin(rads) * translation.x))
+    new_coords.y = origin.y
+    new_coords.rot = origin.rot
+
+    return new_coords
+end
+
 function utils.serializeTable(tab)
     local tabstring = "return {"
     for k, v in pairs( tab ) do
@@ -574,24 +594,4 @@ function utils.serializeTable(tab)
     end
     tabstring = tabstring .. "}"
     return tabstring
-end
-
-local ffxiRotConversionFactor = 360.0 / 255.0
-
-function utils.ffxiRotToDegrees(ffxiRot)
-    return xi.qr_utils.ffxiRotConversionFactor * ffxiRot
-end
-
-
-function utils.lateralTranslateWithOriginRotation(origin, translation)
-    local degrees = xi.qr_utils.ffxiRotToDegrees(origin.rot)
-    local rads = math.rad(degrees)
-    local new_coords = {}
-
-    new_coords.x = origin.x + ((math.cos(rads) * translation.x) + (math.sin(rads) * translation.z))
-    new_coords.z = origin.z + ((math.cos(rads) * translation.z) - (math.sin(rads) * translation.x))
-    new_coords.y = origin.y
-    new_coords.rot = origin.rot
-
-    return new_coords
 end
